49ff0ccb80546d6001ca894cf7839a631b422d0c
rishabgunashekar <rishabgunashekar@gmail.com>
2021-03-06 01:04:40 -0500

    Uploaded twitter sentiment analysis files

---
+++ Lab/Lab 2/Twitter Sentiment Analysis.py
@@ -0,0 +1,105 @@
+#%% Importing required libraries
+import numpy as np
+import pandas as pd
+import re
+from tweepy import API 
+from tweepy import Cursor
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+#To use the twitter credentials of the user to connect to the Twitter API
+import twitter_credentials  
+#%%TWITTER CLIENT
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+#%%TWITTER AUTHENTICATER
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+#This function uses the Twitter credentials which has been stored in the twitter_credentials.py file
+        
+#%% Tweet Analyzer
+        
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweets.
+    """
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
+
+#%% Main Function
+ 
+if __name__ == '__main__':
+
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+
+    api = twitter_client.get_twitter_client_api()
+
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200) #To get Tweets of a particular user for example realDonaldTrump
+    tweets2 = api.search(q='uselections2020', count = 1000) #To search tweets related to the search term
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    df2 = tweet_analyzer.tweets_to_data_frame(tweets2)
+    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
+
+    print(df.head(10))  
+    
+    #Storing results in Excel file
+    df2.to_excel("tweetsusa.xlsx")
+    df.to_excel("results.xlsx")
+
+
